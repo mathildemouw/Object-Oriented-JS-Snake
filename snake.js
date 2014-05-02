184f93e0c16aaaef233fe9c306f39a39758dd097
Mathilde Mouw <nihaomouw@mac.com>
2014-05-02 13:10:49 -0700

    Separate rendering Field into class method rednerEcosystem
    	*next: ecosystem should be superclass that calls methods
    	that render field, food, and snake

--- snake.js
+++ snake.js
@@ -1,27 +1,35 @@
 window.onload = function () {
 
 	var myField = new Field( "snake" );
+	myField.renderEcosystem();
 
 	myField.provideSnakeFood();
 	
-	mySnake = new SnakeController( myField.grassContext );
-	// mySnake.move();
+	mySnake = new SnakeController( myField );
+	mySnake.move();
 
 };
 
 /////////////The Field////////////
 function Field ( canvasId ) {
-	this.grass = document.getElementById( canvasId );
-	this.grassContext = this.grass.getContext( "2d" );
-	this.grassContext.fillStyle = "#AFEEEE";
-	this.grassContext.fillRect( 0, 0, 90, 50 ); //canvasnote: may want to make canvas limits arguments later
+		this.grass = document.getElementById( canvasId );
+		this.grassContext = this.grass.getContext( "2d" );
+		this.foodPosition = {x:10, y:10}
 }
 
 Field.prototype = {
+	renderEcosystem: function () {
+		this.grassContext.fillStyle = "#AFEEEE";
+		this.grassContext.fillRect( 0, 0, 90, 50 );
+		this.grassContext.fillStyle = "#FF"
+		 //canvasnote: may want to make canvas limits arguments later
+	},
 	provideSnakeFood: function () {
 		this.grassContext.fillStyle = "#FF10E0";
 		food = new SnakeFood;
 		food.viewSnakeFood( this.grassContext );
+		this.foodPosition.x = food.xCoord;
+		this.foodPosition.y = food.yCoord;
 	},
 }	
 
@@ -38,13 +46,12 @@ SnakeFood.prototype = {
 }
 
 /////////////The Snake////////////
-function SnakeController ( context ) {
-	this.view = new SnakeView ( context ) ;
-	// this.binder = new SnakeBinder;
+function SnakeController ( fieldForView ) {
+	this.view = new SnakeView ( fieldForView ) ;
+	this.binder = new SnakeBinder;
 	this.model = new SnakeModel;
 }
 SnakeController.prototype = {
-	// associate keyup events with snakeModel functions
 	move: function () {
 		// this.binder.listen();
 		this.view.draw( this.context );
@@ -52,17 +59,18 @@ SnakeController.prototype = {
 	},
 }
 
-// function SnakeBinder () {}
-// SnakeBinder.prototype = {}
+function SnakeBinder () {}
+SnakeBinder.prototype = {}
 
-function SnakeView ( context ) {
-	this.snakeContext = context;
+function SnakeView ( fieldForView ) {
+	this.snakeContext = fieldForView.grassContext;
+	this.wholeField = fieldForView;
 }
 SnakeView.prototype = {
 	// render upon each movement based on model.body coordinates
 	draw: function () {
-		for (i=0, segments = mySnake.model.body; i<segments.length; i++){
-			this.snakeContext.fillStyle = "#FFD0E0"
+		for (var i=0, segments = mySnake.model.body; i<segments.length; i++){
+			this.snakeContext.fillStyle = "#000000"
 			this.snakeContext.fillRect( segments[ i ].x, segments[ i ].y, 2, 2 )
 		}
 	},
@@ -86,7 +94,7 @@ SnakeModel.prototype = {
 			// for ( i=0; i<this.body.length; i++){
 				this.body[ 0 ].x += this.xdirection;
 				this.body[ 0 ].y += this.ydirection;
-				setTimeout()
+				// user setTimeout() to make it sleep between renderings
 				view.draw();
 			// }
 		};
