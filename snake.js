d0995dd7584f9def3cb39921554d3d52e7ead954
Mathilde Mouw <nihaomouw@mac.com>
2014-05-01 17:18:59 -0700

    Create SnakeModel slither method and SnakeView draw method
    	* Draws snake each time it moves
    	* Old position of snake remains on canvas as snake moves
    	* next: change SnakeView draw method to redraw whole canvas

--- snake.js
+++ snake.js
@@ -4,7 +4,7 @@ window.onload = function () {
 
 	myField.provideSnakeFood();
 	
-	mySnake = new SnakeController;
+	mySnake = new SnakeController( myField.grassContext );
 	// mySnake.move();
 
 };
@@ -14,7 +14,7 @@ function Field ( canvasId ) {
 	this.grass = document.getElementById( canvasId );
 	this.grassContext = this.grass.getContext( "2d" );
 	this.grassContext.fillStyle = "#AFEEEE";
-	this.grassContext.fillRect( 0, 0, 90, 50 );
+	this.grassContext.fillRect( 0, 0, 90, 50 ); //canvasnote: may want to make canvas limits arguments later
 }
 
 Field.prototype = {
@@ -27,7 +27,7 @@ Field.prototype = {
 
 /////////////The Food////////////
 function SnakeFood () {
-	this.xCoord = ( Math.floor( Math.random()*88 ) );
+	this.xCoord = ( Math.floor( Math.random()*88 ) ); //canvasnote: should be the limits of the canvas, maybe if instead of while?
 	this.yCoord = ( Math.floor( Math.random()*48 ) );
 }
 
@@ -38,32 +38,40 @@ SnakeFood.prototype = {
 }
 
 /////////////The Snake////////////
-function SnakeController () {
-	this.view = new SnakeView;
-	this.binder = new SnakeBinder;
+function SnakeController ( context ) {
+	this.view = new SnakeView ( context ) ;
+	// this.binder = new SnakeBinder;
 	this.model = new SnakeModel;
 }
 SnakeController.prototype = {
 	// associate keyup events with snakeModel functions
 	move: function () {
 		// this.binder.listen();
-		// this.view.draw();
-		// this.model.slither();
+		this.view.draw( this.context );
+		this.model.slither( this.view );
 	},
 }
 
-function SnakeBinder () {}
-SnakeBinder.prototype = {}
+// function SnakeBinder () {}
+// SnakeBinder.prototype = {}
 
-function SnakeView () {}
+function SnakeView ( context ) {
+	this.snakeContext = context;
+}
 SnakeView.prototype = {
-	// set up rendering upon each movement based on model.body coordinates
+	// render upon each movement based on model.body coordinates
+	draw: function () {
+		for (i=0, segments = mySnake.model.body; i<segments.length; i++){
+			this.snakeContext.fillStyle = "#FFD0E0"
+			this.snakeContext.fillRect( segments[ i ].x, segments[ i ].y, 2, 2 )
+		}
+	},
 }
 
 function SnakeModel () {
 	this.body = [ { x:0, y:0 } ];
 	this.head = this.body[0];
-	this.tail = this.body[this.body.length - 1]
+	this.tail = this.body[this.body.length - 1];
 
 	this.ydirection = 0;
 	this.xdirection = 1;
@@ -72,14 +80,16 @@ function SnakeModel () {
 SnakeModel.prototype = {
 	// while this.body[0] is not intersecting with edge, or intersecting with itself, keep moving in current direction
 	// moves by + or - to x or y coordinate on every elemebt of body array w/ sleep in between
-	alive: function () {},
-	slither: function () {
-		while (this.alive){
-			for ( i=0; i<this.body.length; i++){
-				this.body[ i ].x += this.xdirection;
-				this.body[ i ].y += this.ydirection;
-			}
-		}
+
+	slither: function ( view ) {
+		while (this.head.x < 5){ //canvasnote: should be the limits of the canvas, maybe if instead of while?
+			// for ( i=0; i<this.body.length; i++){
+				this.body[ 0 ].x += this.xdirection;
+				this.body[ 0 ].y += this.ydirection;
+				setTimeout()
+				view.draw();
+			// }
+		};
 	},
 	// turn: function () {},
 	// // when snake head = food square, change food square color to snake color
